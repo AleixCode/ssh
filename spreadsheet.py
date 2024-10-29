f991ff54794a5e9f45bd1ecc8e7a49cb0c298447
Aleix L <aleixcode@gmail.com>
2024-10-29 15:26:01 +0000

    user story 3

--- spreadsheet.py
+++ spreadsheet.py
@@ -12,11 +12,18 @@ class SpreadSheet:
 
     def evaluate(self, cell: str) -> int | str:
         value = self.get(cell)
-        try:
-            return int(value)
-        except ValueError:
-            if value.startswith("'") and value.endswith("'"):
-                return value[1:-1]
-            else:
+        if value.startswith("="):
+            try:
+                if value[1:].startswith("'") and value[1:].endswith("'"):
+                    return value[2:-1]
+                else:
+                    return int(value[1:])
+            except ValueError:
                 return "#Error"
+        elif value.startswith("'") and value.endswith("'"):
+            return value[1:-1]
+        elif value.isdigit():
+            return int(value)
+        else:
+            return "#Error"
 
--- test/testspreadsheet.py
+++ test/testspreadsheet.py
@@ -4,6 +4,21 @@ from spreadsheet import SpreadSheet
 
 class TestSpreadSheet(TestCase):
 
+    def test_evaluate_formula_string(self):
+        spreadsheet = SpreadSheet()
+        spreadsheet.set("A1",   "='Apple'")
+        self.assertEqual("Apple", spreadsheet.evaluate("A1"))
+
+    def test_evaluate_formula_integer(self):
+        spreadsheet = SpreadSheet()
+        spreadsheet.set("A1",  "=1")
+        self.assertEqual(1, spreadsheet.evaluate("A1"))
+
+    def test_evaluate_formula_wrong_string(self):
+        spreadsheet = SpreadSheet()
+        spreadsheet.set("A1",  "='Apple")
+        self.assertEqual("#Error", spreadsheet.evaluate("A1"))
+
     def test_evaluate_invalid_string_2(self):
         spreadsheet = SpreadSheet()
         spreadsheet.set("B1", "Apple'")
